43164bfb6cdf8d16d0e4e28171ae4e9b25addf09
Gabriel Andrade <gabriel.andrade@azultecnologia.com>
2025-08-28 16:54:51 -0300

    Add user existence check and login functionality in UserService

--- fintrack-api/src/main/java/com/fintrack/fintrack_api/services/UserService.java
+++ fintrack-api/src/main/java/com/fintrack/fintrack_api/services/UserService.java
@@ -1,10 +1,13 @@
 package com.fintrack.fintrack_api.services;
 
+import com.fintrack.fintrack_api.DTOs.LoginDTO;
 import com.fintrack.fintrack_api.DTOs.UserDTO;
 import com.fintrack.fintrack_api.config.JwtConfigToken;
 import com.fintrack.fintrack_api.entities.User;
 import com.fintrack.fintrack_api.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -20,13 +23,39 @@ public class UserService {
     @Autowired
     private JwtConfigToken createJWT;
 
-    public String createUser(UserDTO userDTO) {
-        String senhaHash = bCryptPasswordEncoder.encode(userDTO.getPassword());
-        String token = createJWT.generateToken(userDTO.getEmail());
-        userDTO.setPassword(senhaHash);
-//        User userSave = new User(userDTO);
-//        this.userRepository.save(userSave);
+    public ResponseEntity<String> createUser(User user) {
 
-        return token;
+        boolean userExists = this.userRepository.existsByEmail(user.getEmail());
+        if (userExists) {
+            return ResponseEntity.status(HttpStatus.CONFLICT).body("Email ja existe!");
+        }
+
+        String senhaHash = bCryptPasswordEncoder.encode(user.getPassword());
+        user.setPassword(senhaHash);
+        this.userRepository.save(user);
+        UserDTO userDTO = new UserDTO(user);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body("Usuário criado com sucesso.");
+    }
+
+    public ResponseEntity<LoginDTO> login(LoginDTO loginDTO) {
+
+        if (loginDTO.getEmail() == null || loginDTO.getPassword() == null) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
+        }
+
+        User existingUser = userRepository.findByEmail(loginDTO.getEmail());
+        if (existingUser == null) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(null);
+        }
+
+        boolean passwordMatches = bCryptPasswordEncoder.matches(loginDTO.getPassword(), existingUser.getPassword());
+        if (!passwordMatches) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(null);
+        }
+
+        String token = createJWT.generateToken(existingUser.getEmail());
+        LoginDTO responseDTO = new LoginDTO(existingUser, token);
+        return ResponseEntity.ok(responseDTO);
     }
 }
