09aa5c2550ed59db11476aac9996dd4d9505f231
Gabriel Andrade <gabriel.andrade@azultecnologia.com>
2025-08-28 16:54:06 -0300

    Add LoginDTO to encapsulate login-related data

---
+++ fintrack-api/src/main/java/com/fintrack/fintrack_api/DTOs/LoginDTO.java
@@ -0,0 +1,30 @@
+package com.fintrack.fintrack_api.DTOs;
+
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fintrack.fintrack_api.entities.User;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class LoginDTO {
+    private String name;
+    private String email;
+
+    @JsonIgnore
+    private String password;
+    private String token;
+
+    public LoginDTO(User existingUser, String token) {
+        this.email = existingUser.getEmail();
+        this.name = existingUser.getName();
+        this.token = token;
+
+    }
+
+}
