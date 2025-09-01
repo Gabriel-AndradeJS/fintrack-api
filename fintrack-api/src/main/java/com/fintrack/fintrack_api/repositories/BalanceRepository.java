56146c01c5bd29e03c07e6014c56c2e9d550c32a
Gabriel Andrade <gabriel.andrade@azultecnologia.com>
2025-09-01 17:23:23 -0300

    Remove Balance entity and BalanceRepository

--- fintrack-api/src/main/java/com/fintrack/fintrack_api/repositories/BalanceRepository.java
+++
@@ -1,7 +0,0 @@
-package com.fintrack.fintrack_api.repositories;
-
-import com.fintrack.fintrack_api.entities.Balance;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BalanceRepository extends JpaRepository<Balance, Long> {
-}
