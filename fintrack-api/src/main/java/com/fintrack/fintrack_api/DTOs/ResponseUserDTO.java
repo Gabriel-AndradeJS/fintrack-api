b673cfa32f45bbd0d61c03251b9063e18e2c97a4
Gabriel Andrade <gabriel.andrade@azultecnologia.com>
2025-09-01 17:23:38 -0300

    Add ResponseUserDTO for user data encapsulation; update ResponseLoginDTO with balance attributes

--- fintrack-api/src/main/java/com/fintrack/fintrack_api/DTOs/ResponseUserDTO.java
+++ fintrack-api/src/main/java/com/fintrack/fintrack_api/DTOs/ResponseUserDTO.java
@@ -1,6 +1,5 @@
 package com.fintrack.fintrack_api.DTOs;
 
-
 import com.fintrack.fintrack_api.entities.Expenses;
 import com.fintrack.fintrack_api.entities.User;
 import lombok.AllArgsConstructor;
@@ -8,24 +7,31 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
-public class ResponseLoginDTO {
+public class ResponseUserDTO {
+    private Long id;
     private String name;
     private String email;
     private Double totalBalance;
     private Double currentBalance;
-    private String token;
+    private List<Expenses> expenses;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
 
-    public ResponseLoginDTO(User user, String token) {
+    public ResponseUserDTO(User user) {
+        this.id = user.getId();
         this.name = user.getName();
         this.email = user.getEmail();
         this.totalBalance = user.getBalance();
-        this.currentBalance = user.getBalance();
-        this.token = token;
+        this.currentBalance = user.getCurrentBalance();
+        this.createdAt = user.getCreatedAt();
+        this.updatedAt = user.getUpdateAt();
+        this.expenses = user.getExpenses();
     }
 }
